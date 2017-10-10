e00be5102256ca2a140c897a8e0f04e34326281a
Hanoch Moreno <hanoch001@gmail.com>
2017-10-10 10:16:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hanoc_000/countriesmaccabi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hanoc_000.countriesmaccabi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
